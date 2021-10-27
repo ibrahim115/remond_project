4ce203284a900bf2f3a9c1a9d5b71e739289037f
ibrahim115 <49085486+ibrahim115@users.noreply.github.com>
2021-10-27 20:55:43 +0700

    Revisi commit video baru 10

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,15 +7,16 @@ const { check, validationResult } = require('express-validator');
 // @access Public
 
 router.post('/', [
-  check ('name', 'Nama harus di isi').not().isEmpty(),
-  check ('email', 'isi dengan valid email').isEmail(),
-  check ('password', 'Isi password minimal 6 atau lebih karakter').isLength({ min: 6 })],
-(req, res) => {
-  // handle request
-  const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+  // .not().is Empty => validation!
+  check('name', 'Nama harus di isi').not().isEmpty(),
+  check('email', 'Isi dengan valid email').isEmail(),
+  check('password', 'Isi password minimal 6 atau lebih karakter').isLength({min: 6})],
+  (req,res) => {
+    // handle request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {return res.status(400).json({errors: errors.array() });
+  }
+
   res.send('User route')
 });
 
