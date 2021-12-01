16e22fe7828b00d3d6f39b289bf65484926cd019
ibrahim115 <49085486+ibrahim115@users.noreply.github.com>
2021-12-01 23:08:23 +0700

    Commit video 13

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/Users');
 
 // @routeGET api/auth
 // @descTest route
 // @accessPublic atau Private
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
+
 module.exports = router;
