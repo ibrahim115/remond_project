f8d125101a9073cc265da565b5133a3a12d6cbbd
ibrahim115 <49085486+ibrahim115@users.noreply.github.com>
2022-01-14 04:23:43 +0700

    Commit video 16

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @routeGET api/profile
-// @descTest route
-// @accessPublic atau Private
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
 
-router.get('/', (req, res) => res.send('Profile route'));
+// @routeGET api/profile/me
+// @descTest get current users profile
+// @access Private
+
+router.get('/me',auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate ('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Tidak ada profile untuk user ini' });
+    }
+
+    res.json(profile);
+
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// export route
 module.exports = router;
