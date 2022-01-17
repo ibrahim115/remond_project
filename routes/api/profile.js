2dcaee6206d75926f591bc4ff85d6726e4c20b6f
ibrahim115 <49085486+ibrahim115@users.noreply.github.com>
2022-01-17 22:49:27 +0700

    Commit video 17

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const {check, validationResult} = require ('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/Users');
+const { compareSync } = require('bcryptjs');
 
 // @routeGET api/profile/me
 // @descTest get current users profile
@@ -23,6 +25,57 @@ router.get('/me',auth, async (req, res) => {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
+});
+
+// @route  Post api/profile/me
+// @desc   Create or Update users profile
+// @access Private
+
+router.post('/', [
+  auth, 
+  [
+  check('status', 'Status is required')
+    .not()
+    .notEmpty(),
+  ] 
+                ], 
+async (req,res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    company,
+    website,
+    location,
+    bio,
+    status,
+    githubusername,
+    skills,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedln
+        } = req.body;
+
+    //build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+    console.log(profileFields.skills);
+    res.send(profileFields.skills);
+
 });
 
 // export route
