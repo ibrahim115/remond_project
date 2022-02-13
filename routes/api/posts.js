6f68fe5d575a4cb6ac65ef0cc7df99ca317d7f33
ibrahim115 <49085486+ibrahim115@users.noreply.github.com>
2022-02-13 20:51:50 +0700

    Commit video 25 & 26

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
+const auth = require('../../middleware/auth');
+
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
 
 // @routeGET api/posts
 // @descTest route
 // @access Public atau Private
 
-router.get('/', (req,res) => res.send('Posts route'));
+// router.get('/', (req,res) => res.send('Posts route'));
+
+// @route POST api/posts
+// @desc Create a post
+// @access Private
+
+router.post('/',
+[
+  auth,
+  [check('text', 'Text is required')
+.not()
+.isEmpty()
+]
+],
+async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try{
+    //retrieve user by Id tanpa password
+    const user = await User.findById(req.user.id).select('-password');
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    });
+
+    const post = await newPost.save();
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+}
+);
 
 
 // export route
